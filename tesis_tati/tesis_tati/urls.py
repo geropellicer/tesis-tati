047ea5d794a03fce52830efe2756a6c9d8e14987
geropellicer <geropellicer@gmail.com>
2021-09-30 19:41:46 -0300

    Addes static files urls

--- tesis_tati/tesis_tati/urls.py
+++ tesis_tati/tesis_tati/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from comments.views import CommentViewSet
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 router = routers.DefaultRouter()
 router.register(r'comments', CommentViewSet)
@@ -25,3 +26,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
+urlpatterns += staticfiles_urlpatterns()
