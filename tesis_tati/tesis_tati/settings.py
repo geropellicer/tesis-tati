27fe9823893a6b6de998d837c0db1a0e6b31f3ee
geropellicer <geropellicer@gmail.com>
2021-09-30 19:37:16 -0300

    Updated static root

--- tesis_tati/tesis_tati/settings.py
+++ tesis_tati/tesis_tati/settings.py
@@ -140,5 +140,4 @@ REST_FRAMEWORK = {
 }
 
 # All settings common to all environments
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
